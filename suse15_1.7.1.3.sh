8b2f576b20ff39117310ec09327a4daba2cf78a2
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:18:34 +0530

    Update and rename suse12_1.6.1.3.sh to suse15_1.7.1.3.sh

--- suse15_1.7.1.3.sh
+++ suse15_1.7.1.3.sh
@@ -2,5 +2,5 @@
 
 # Ensure all AppArmor Profiles are in enforce or complain mode
 echo
-echo \*\*\*\* 1.6.1.3 Ensure all AppArmor Profiles are in enforce or complain mode
+echo \*\*\*\* 1.7.1.3 Ensure all AppArmor Profiles are in enforce or complain mode
 aa-enforce /etc/apparmor.d/*
