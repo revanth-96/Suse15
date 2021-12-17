d3faed5604090d2adbe85dafc5499286bbbca482
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:53:58 +0530

    Add files via upload

---
+++ suse12_3.2.5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 3.2.5 Ensure broadcast ICMP requests are ignored
+echo
+echo \*\*\*\* 3.2.5 Ensure broadcast ICMP requests are ignored
+egrep -q "^(\s*)net.ipv4.icmp_echo_ignore_broadcasts\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.icmp_echo_ignore_broadcasts\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.icmp_echo_ignore_broadcasts = 1\2/" /etc/sysctl.conf || echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.conf
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1 
+sysctl -w net.ipv4.route.flush=1
---
+++ suse12_3.2.6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 3.2.6 Ensure bogus ICMP responses are ignored
+echo
+echo \*\*\*\* 3.2.6 Ensure bogus ICMP responses are ignored
+egrep -q "^(\s*)net.ipv4.icmp_ignore_bogus_error_responses\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri"s/^(\s*)net.ipv4.icmp_ignore_bogus_error_responses\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.icmp_ignore_bogus_error_responses = 1\2/" /etc/sysctl.conf || echo "net.ipv4.icmp_ignore_bogus_error_responses = 1" >> /etc/sysctl.conf
+sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1 
+sysctl -w net.ipv4.route.flush=1
