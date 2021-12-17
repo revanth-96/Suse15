fdb6648441d9cb342294cb4a9e04c6a468f860d8
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:22:29 +0530

    Update and rename suse12_1.7.1.4.sh to suse15_1.8.1.4.sh

--- suse15_1.8.1.4.sh
+++ suse15_1.8.1.4.sh
@@ -2,7 +2,7 @@
 
 # Ensure permissions on /etc/motd are configured
   echo
-  echo \*\*\*\* 1.7.1.4 Ensure permissions on /etc/motd are configured
+  echo \*\*\*\* 1.8.1.4 Ensure permissions on /etc/motd are configured
   #touch /etc/motd
   chown root:root /etc/motd
   chmod u-x,go-wx /etc/motd
