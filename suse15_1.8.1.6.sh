e3e4292275f26253370da0a138d5ad3dd6823896
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:23:27 +0530

    Update and rename suse12_1.7.1.6.sh to suse15_1.8.1.6.sh

--- suse15_1.8.1.6.sh
+++ suse15_1.8.1.6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Ensure permissions on /etc/issue.net are configured
-echo \*\*\*\* 1.7.1.6 Ensure permissions on /etc/issue.net are configured
+echo \*\*\*\* 1.8.1.6 Ensure permissions on /etc/issue.net are configured
 chown root:root /etc/issue.net
 chmod u-x,go-wx /etc/issue.net
