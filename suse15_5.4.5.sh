1177dca1d6a116865fb26ea109af44f331710687
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 16:04:07 +0530

    Update and rename suse12_5.5.5.sh to suse15_5.4.5.sh

--- suse15_5.4.5.sh
+++ suse15_5.4.5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 5.5.5 Ensure default user umask is configured
-echo "\*\*\*\* 5.5.5 Ensure\ default\ user\ umask is configured"
+# 5.4.5 Ensure default user umask is configured
+echo "\*\*\*\* 5.4.5 Ensure\ default\ user\ umask is configured"
 touch /etc/bash.bashrc.local
 touch /etc/profile.local 
 grep "umask" /etc/bash.bashrc.local && sed -i 's/.*umask.*/umask 027/' /etc/bash.bashrc.local || echo "umask 027" >> /etc/bash.bashrc.local
