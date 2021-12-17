102603f4688d6f068b7e425592555e5e2243354a
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 16:02:46 +0530

    Update and rename suse12_5.5.2.sh to suse15_5.4.2.sh

--- suse15_5.4.2.sh
+++ suse15_5.4.2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 5.5.2 Ensure system accounts are secured (Automated)
-echo "\*\*\*\* 5.5.2 Ensure system accounts are secured (Automated)"
+echo "\*\*\*\* 5.4.2 Ensure system accounts are secured (Automated)"
 #	following	command	will	set	all	system	accounts	to	a	non	login	shell
 awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $7!="'"$(which nologin)"'" && $7!="/bin/false" && $7!="/usr/bin/false") {print $1}' /etc/passwd | while read -r user; do usermod -s "$(which nologin)" "$user"; done
 #following	command	will	automatically	lock	not	root	system	accounts
