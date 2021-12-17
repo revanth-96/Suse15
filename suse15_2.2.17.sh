00410f5cb31c399f1a06c7d1faf732d789b4822b
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:42:10 +0530

    Update and rename suse12_2.2.18.sh to suse15_2.2.17.sh

--- suse15_2.2.17.sh
+++ suse15_2.2.17.sh
@@ -2,5 +2,5 @@
 
 # Ensure rsync is not installed or the rsyncd service is masked
   echo
-  echo \*\*\*\* 2.2.18 Ensure rsync is not installed or the rsyncd service is masked
+  echo \*\*\*\* 2.2.17 Ensure rsync is not installed or the rsyncd service is masked
   echo y | zypper -n remove rsync
