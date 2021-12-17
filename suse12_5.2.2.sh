1bb026509970ce7af512cfe51e3a9d1649768c9b
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 15:36:18 +0530

    Delete suse12_5.2.2.sh

--- suse12_5.2.2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 5.2.2 Ensure SSH Protocol is set to 2
-echo
-echo \*\*\*\* 5.2.2 Ensure SSH Protocol is set to 2
-grep "Protocol" /etc/ssh/sshd_config && sed -i '/Protocol/s/#//' /etc/ssh/sshd_config || echo "Protocol 2" >> /etc/ssh/sshd_config
