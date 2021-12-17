1cb46534818a6741a6c72f6779bd6b9c4afc025d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:17:39 +0530

    Update and rename suse12_1.6.1.1.sh to suse15_1.7.1.1.sh

--- suse15_1.7.1.1.sh
+++ suse15_1.7.1.1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo \*\*\*\* 1.6.1.1 Ensure AppArmor is installed
+echo \*\*\*\* 1.7.1.1 Ensure AppArmor is installed
 echo y | zypper install -t pattern apparmor
 #asks for continue
