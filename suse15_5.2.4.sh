66729c5e987359563f1aed5b4d7fabc884b4a1c3
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 15:33:14 +0530

    Update and rename suse12_5.2.14.sh to suse15_5.2.4.sh

--- suse15_5.2.4.sh
+++ suse15_5.2.4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 5.2.14 Ensure SSH access is limited
+# 5.2.4 Ensure SSH access is limited
 echo
-echo \*\*\*\* 5.2.15 Ensure SSH access is limited
+echo \*\*\*\* 5.2.4 Ensure SSH access is limited
 grep "^AllowUsers" /etc/ssh/sshd_config || echo "AllowUsers " >> /etc/ssh/sshd_config
 grep "^AllowGroups" /etc/ssh/sshd_config || echo "AllowGroups " >> /etc/ssh/sshd_config
 grep "^DenyUsers" /etc/ssh/sshd_config || echo "DenyUsers " >> /etc/ssh/sshd_config
