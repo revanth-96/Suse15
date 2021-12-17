dad7b899cd4aafda6400a7e6e32c55a674b508ef
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:39:15 +0530

    Update and rename suse12_2.2.16.sh to suse15_2.2.7.sh

--- suse15_2.2.7.sh
+++ suse15_2.2.7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #  Ensure nfs-utils is not installed or the nfs-server service is masked
-echo \*\*\*\* 2.2.16  Ensure nfs-utils is not installed or the nfs-server service is masked
+echo \*\*\*\* 2.2.7  Ensure nfs-utils is not installed or the nfs-server service is masked
 # echo y | zypper -n remove zypper remove nfs-utils
   echo y | zypper -n remove nfs-kernel-server
