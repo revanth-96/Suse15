bcf9d4fe4692c6e3e4e61a8cde656a131eccaa8c
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 15:30:50 +0530

    Update and rename suse12_5.2.11.sh to suse15_5.2.14.sh

--- suse15_5.2.14.sh
+++ suse15_5.2.14.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 5.2.11 Ensure only approved MAC algorithms are used
+# 5.2.14 Ensure only approved MAC algorithms are used
 echo
-echo \*\*\*\* 5.2.11 Ensure only approved MAC algorithms are used
+echo \*\*\*\* 5.2.14 Ensure only approved MAC algorithms are used
 grep "MACs" /etc/ssh/sshd_config && sed -i "s/.*MACs.*/MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com/" /etc/ssh/sshd_config || echo "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com" >> /etc/ssh/sshd_config
