75b2fdcad898e65868845efdc502ef704b95a10d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:21:24 +0530

    Update and rename suse12_1.7.1.2.sh to suse15_1.8.1.2.sh

--- suse15_1.8.1.2.sh
+++ suse15_1.8.1.2.sh
@@ -2,5 +2,5 @@
 
 # Ensure local login warning banner is configured properly
   echo
-  echo \*\*\*\* 1.7.1.2 Ensure local login warning banner is configured properly
+  echo \*\*\*\* 1.8.1.2 Ensure local login warning banner is configured properly
   echo "Authorized uses only. All activity may be monitored and reported." > /etc/issue
