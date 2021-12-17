34ececc527dae91bbb3d0630e658591c77c014be
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 15:52:37 +0530

    Add files via upload

---
+++ suse12_5.3.4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 5.3.4 Ensure password hashing algorithm is SHA-512
+echo
+echo \*\*\*\* 5.3.4 Ensure password hashing algorithm is SHA-512
+#egrep -q "^\s*password\s+sufficient\s+pam_unix.so\s+" /etc/pam.d/common-password && sed -ri '/^\s*password\s+sufficient\s+pam_unix.so\s+/ { /^\s*password\s+sufficient\s+pam_unix.so(\s+\S+)*(\s+sha512)(\s+.*)?$/! s/^(\s*password\s+sufficient\s+pam_unix.so\s+)(.*)$/\1sha512 \2/ }' /etc/pam.d/common-password || echo "password required pam_unix.so sha512" >> /etc/pam.d/common-password
+sed -i '/pam_unix.so/ s/$/ sha512/' /etc/pam.d/common-password
