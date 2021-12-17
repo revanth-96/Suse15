b2cb3e820b1dbdd78bf0013eaed130f4fa087b75
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 15:42:41 +0530

    Update and rename suse12_5.2.8.sh to suse15_5.2.10.sh

--- suse15_5.2.10.sh
+++ suse15_5.2.10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 5.2.8 Ensure SSH root login is disabled
+# 5.2.10 Ensure SSH root login is disabled
 echo
-echo \*\*\*\* 5.2.8 Ensure SSH root login is disabled
+echo \*\*\*\* 5.2.10 Ensure SSH root login is disabled
 egrep -q "^(\s*)PermitRootLogin\s+\S+(\s*#.*)?\s*$" /etc/ssh/sshd_config && sed -ri "s/^(\s*)PermitRootLogin\s+\S+(\s*#.*)?\s*$/\1PermitRootLogin no\2/" /etc/ssh/sshd_config || echo "PermitRootLogin no" >> /etc/ssh/sshd_config
