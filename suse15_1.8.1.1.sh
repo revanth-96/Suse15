18cc7fc9b974ec9b49b33938f3218b492811d0f1
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:20:59 +0530

    Update and rename suse12_1.7.1.1.sh to suse15_1.8.1.1.sh

--- suse15_1.8.1.1.sh
+++ suse15_1.8.1.1.sh
@@ -2,6 +2,6 @@
 
 # Ensure message of the day is configured properly
   echo
-  echo \*\*\*\* 1.7.1.1 Ensure message of the day is configured properly
+  echo \*\*\*\* 1.8.1.1 Ensure message of the day is configured properly
   [ -s /etc/motd ] || echo "All activities performed on this system will be monitored." > /etc/motd
   sed -ri 's/(\\v|\\r|\\m|\\s)//g' /etc/motd
