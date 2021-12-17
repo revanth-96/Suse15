94204335121cfbe0033b9bc29fbbcfdc3e17a365
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:41:34 +0530

    Update and rename suse12_2.2.17.sh to suse15_2.2.8.sh

--- suse15_2.2.8.sh
+++ suse15_2.2.8.sh
@@ -2,5 +2,5 @@
 
 #  Ensure rpcbind is not installed or the rpcbind services are masked
   echo
-  echo \*\*\*\* 2.2.17  Ensure rpcbind is not installed or the rpcbind services are masked
+  echo \*\*\*\* 2.2.8  Ensure rpcbind is not installed or the rpcbind services are masked
   echo y | zypper -n remove rpcbind
