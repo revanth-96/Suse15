1c4b971a36de7e4c36c33b039031d5e59ac1dbf4
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:58:23 +0530

    Update and rename suse12_3.2.8.sh to suse15_3.3.8.sh

--- suse15_3.3.8.sh
+++ suse15_3.3.8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 3.2.8 Ensure TCP SYN Cookies is enabled
+# 3.3.8 Ensure TCP SYN Cookies is enabled
 echo
-echo \*\*\*\* 3.2.8 Ensure\ TCP\ SYN\ Cookies\ is\ enabled
+echo \*\*\*\* 3.3.8 Ensure\ TCP\ SYN\ Cookies\ is\ enabled
 egrep -q "^(\s*)net.ipv4.tcp_syncookies\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.tcp_syncookies\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.tcp_syncookies = 1\2/" /etc/sysctl.conf || echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
 sysctl -w net.ipv4.tcp_syncookies=1  
 sysctl -w net.ipv4.route.flush=1
