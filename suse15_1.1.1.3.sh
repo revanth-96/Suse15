bc20bff7a6089bf8a20725642f1dcd78b918a409
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 18:21:49 +0530

    Update suse15_1.1.1.3.sh

--- suse15_1.1.1.3.sh
+++ suse15_1.1.1.3.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-"1.1.1.3 Ensure mounting of FAT filesystems is limited (Manual)" 
+ 
+# 1.1.1.3 Ensure mounting of Fat filesystems is disabled
+echo
+echo \*\*\*\* 1.1.1.3 Ensure mounting of Fat filesystems is disabled
+modprobe -n -v fat | grep "install /bin/true" || echo "install fat/bin/true" >> /etc/modprobe.d/fat.conf
+lsmod | egrep "fat" && rmmod fat
+
+modprobe -n -v vfat | grep "install /bin/true" || echo "install vfat/bin/true" >> /etc/modprobe.d/vfat.conf
+lsmod | egrep "vfat" && rmmod vfat
+
+modprobe -n -v msdos| grep "install /bin/true" || echo "install msdos/bin/true" >> /etc/modprobe.d/msdos.conf
+lsmod | egrep "msdos" && rmmod msdos
