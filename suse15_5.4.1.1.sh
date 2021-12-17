994c9c727c6caadfdde7c457110a40a87e2b014e
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 15:53:43 +0530

    Update and rename suse12_5.3.4.sh to suse15_5.4.1.1.sh

--- suse15_5.4.1.1.sh
+++ suse15_5.4.1.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 5.3.4 Ensure password hashing algorithm is SHA-512
+# 5.4.1.1 Ensure password hashing algorithm is SHA-512
 echo
-echo \*\*\*\* 5.3.4 Ensure password hashing algorithm is SHA-512
+echo \*\*\*\* 5.4.1.1 Ensure password hashing algorithm is SHA-512
 #egrep -q "^\s*password\s+sufficient\s+pam_unix.so\s+" /etc/pam.d/common-password && sed -ri '/^\s*password\s+sufficient\s+pam_unix.so\s+/ { /^\s*password\s+sufficient\s+pam_unix.so(\s+\S+)*(\s+sha512)(\s+.*)?$/! s/^(\s*password\s+sufficient\s+pam_unix.so\s+)(.*)$/\1sha512 \2/ }' /etc/pam.d/common-password || echo "password required pam_unix.so sha512" >> /etc/pam.d/common-password
 sed -i '/pam_unix.so/ s/$/ sha512/' /etc/pam.d/common-password
