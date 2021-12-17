aa49fb70a24344c1474215a83fb36e42f21151e6
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:29:42 +0530

    Add files via upload

---
+++ suse12_1.8.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Ensure updates, patches, and additional security software are installed
+  echo
+  echo \*\*\*\* 1.8 Ensure updates, patches and additional security software are installed
+  echo y | zypper -n update
