17b0e6871e7f729d92483b206e691c4cc972fa1c
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 13:06:39 +0530

    Add files via upload

---
+++ suse12_1.4.2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+# 1.4.1 Ensure permissions on bootloader config are configured
+echo
+echo \*\*\*\* 1.4.1 Ensure permissions on bootloader config are configured
+chmod 600 /boot/grub2/grub.cfg
---
+++ suse12_1.4.3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+
+# Ensure authentication required for single user mode
+  echo
+  echo \*\*\*\* 1.4.3 Ensure authentication required for single user mode
+  grep -q "^\s*ExecStart" /usr/lib/systemd/system/rescue.service && sed -ri "s/(^[[:space:]]*ExecStart[[:space:]]*=[[:space:]]*).*$/\1-\/bin\/sh -c \"\/sbin\/sulogin; \/usr\/bin\/systemctl --fail --no-block default\"/" /usr/lib/systemd/system/rescue.service || echo "ExecStart=-/bin/sh -c \"/sbin/sulogin; /usr/bin/systemctl --fail --no-block default\"" >> /usr/lib/systemd/system/rescue.service
+  grep -q "^\s*ExecStart" /usr/lib/systemd/system/emergency.service && sed -ri "s/(^[[:space:]]*ExecStart[[:space:]]*=[[:space:]]*).*$/\1-\/bin\/sh -c \"\/sbin\/sulogin; \/usr\/bin\/systemctl --fail --no-block default\"/" /usr/lib/systemd/system/emergency.service || echo "ExecStart=-/bin/sh -c \"/sbin/sulogin; /usr/bin/systemctl --fail --no-block default\"" >> /usr/lib/systemd/system/emergency.service
