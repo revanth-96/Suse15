bd2220facd2225c5a5d933dc4de3049d7e88b1e8
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2021-12-17 18:28:02 +0530

    Update suse15_1.3.2.sh

--- suse15_1.3.2.sh
+++ suse15_1.3.2.sh
@@ -1,1 +1,6 @@
-1.3.2 Ensure sudo commands use pty
+#!/bin/bash
+
+# Ensure sudo commands use pty --check
+  echo
+  echo \*\*\*\* 1.3.2 Ensure sudo commands use pty
+  grep -Ei '^\s*Defaults\s+([^#]+,\s*)?use_pty(,\s+\S+\s*)*(\s+#.*)?$' /etc/sudoers /etc/sudoers.d/* || echo "Defaults use_pty" >> /etc/sudoers
